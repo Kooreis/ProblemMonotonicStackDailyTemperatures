2658f848965f9c9a68d4f5c755e607c3645fe225
Kooreis <kooreis.official@gmail.com>
2024-07-10 04:39:12 -0500

    In this update, the necessary libraries are imported and the main function is defined. The user is prompted to enter the number of days and the temperatures for those days, which are stored in an array.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that uses a monotonic stack to solve the daily temperatures problem. The problem is to find out how many days you would have to wait until a warmer temperature for each day.
-
-```java
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
@@ -15,26 +12,5 @@ public class Main {
         for (int i = 0; i < n; i++) {
             temperatures[i] = scanner.nextInt();
         }
-        int[] result = dailyTemperatures(temperatures);
-        System.out.println("Days to wait for warmer temperature:");
-        for (int i : result) {
-            System.out.println(i);
-        }
-    }
-
-    public static int[] dailyTemperatures(int[] T) {
-        int[] result = new int[T.length];
-        Stack<Integer> stack = new Stack<>();
-        for (int i = T.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && T[i] >= T[stack.peek()]) {
-                stack.pop();
-            }
-            result[i] = stack.isEmpty() ? 0 : stack.peek() - i;
-            stack.push(i);
-        }
-        return result;
     }
 }
-```
-
-This program first takes the number of days and the temperatures for those days as input. It then uses a monotonic stack to find out how many days you would have to wait until a warmer temperature for each day. The results are then printed to the console.
