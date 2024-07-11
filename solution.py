3d5c0e02a3f776baff252bf2af95b908ca797b46
Kooreis <kooreis.official@gmail.com>
2024-07-11 05:39:12 -0500

    In the second update, a for loop is added to iterate over the input list 'T' in reverse order. Inside this loop, a while loop is added to pop elements from the 'stack' as long as the 'stack' is not empty and the current element in 'T' is greater than or equal to the last element in the 'stack'.

--- solution.py
+++ solution.py
@@ -1,4 +1,3 @@
-class Solution:
-    def dailyTemperatures(self, T):
-        ans = [0] * len(T)
-        stack = []
+for i in range(len(T) - 1, -1, -1):
+            while stack and T[i] >= T[stack[-1]]:
+                stack.pop()
