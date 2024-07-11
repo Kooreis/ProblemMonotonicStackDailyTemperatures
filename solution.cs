0c8979145f21e0f6a75a4348ca2518be16ad11a9
Kooreis <kooreis.official@gmail.com>
2024-07-11 05:39:12 -0500

    In this update, the DailyTemperatures function is called with the temperatures array as an argument. The result is stored in a new array.

--- solution.cs
+++ solution.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main(string[] args)
+static void Main(string[] args)
     {
         int[] temperatures = { 73, 74, 75, 71, 69, 72, 76, 73 };
+        int[] result = DailyTemperatures(temperatures);
     }
-}
